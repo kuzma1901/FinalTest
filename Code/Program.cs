cf6fbfbf5a275789ac9a0d6fc6f84f59a8802643
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-11-11 11:14:28 +0300

    Написал программу для решения поставленной задачи

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string[] firstArray = new string[4] {"-35", "AAA", "Russia", "1234"};
+string[] secondArray = new string[4];
+void SecondArrayFind(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+    if(firstArray[i].Length <= 3)
+        {
+        secondArray[count] = firstArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayFind(firstArray, secondArray);
+PrintArray(secondArray);
