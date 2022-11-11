3400704d581d1bc8e59a7de004e3700aa2f9e826
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-11-11 11:40:46 +0300

    сделал автоформатирование кода

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,24 +1,30 @@
-﻿string[] firstArray = new string[4] {"-35", "AAA", "Russia", "1234"};
+﻿string[] firstArray = new string[4] { "-35", "AAA", "Russia", "1234" };
 string[] secondArray = new string[4];
+
 void SecondArrayFind(string[] firstArray, string[] secondArray)
 {
     int count = 0;
     for (int i = 0; i < firstArray.Length; i++)
     {
-    if(firstArray[i].Length <= 3)
+        if (firstArray[i].Length <= 3)
         {
-        secondArray[count] = firstArray[i];
-        count++;
+            secondArray[count] = firstArray[i];
+            count++;
         }
     }
 }
+
 void PrintArray(string[] array)
 {
+    Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+
+        Console.Write($" {array[i]}");
+
     }
-    Console.WriteLine();
+    Console.Write("]");
 }
+
 SecondArrayFind(firstArray, secondArray);
 PrintArray(secondArray);
